694e65ff2bf43ce1e23648e1b2f361398f8da06c
WebDev2105 <choudareddy9842@gmail.com>
2024-10-13 16:13:29 +0530

    Initial Set Up is Ready

---
+++ src/Components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="grid grid-flow-col shadow-xl p-4">
+      <div className="flex h-10 grid-cols-1">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/021/190/402/non_2x/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg"
+          alt="menu-icon"
+        />
+        <img
+          src="https://t4.ftcdn.net/jpg/06/09/13/31/360_F_609133122_u5vaiyCHsL5UqtdrSNrV5llL2JC1oYWi.jpg"
+          alt="youtube-logo"
+        />
+      </div>
+      <div className="grid-cols-10 h-10">
+        <input
+          type="text"
+          className="w-96 rounded-l-full p-1 border border-gray-400 border-solid"
+          placeholder="Search..."
+        />
+        <button className="rounded-r-full p-1 border  border-gray-400 border-solid">
+          🔍
+        </button>
+      </div>
+      <div className="grid-cols-1 ">
+        <img
+          className="h-10 float-end "
+          src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png"
+          alt="user-icon"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/SideBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="w-48 shadow-xl p-4">
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Videos</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold mt-4">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold mt-4">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
